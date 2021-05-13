8db8eb0c6b98b22f0d2a45f09dccac6029ad404d
Vladyslav-S <82597317+Vladyslav-S@users.noreply.github.com>
2021-05-13 17:34:30 +0300

    CustomCell
    -Create and add a custom tableViewCell (xib&nib)
    -configure a table view

---
+++ pProjectAllTasks/Lesson-15/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  pProjectAllTasks
+//
+//  Created by MACsimus on 13.05.2021.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String
+    let body: String
+}
